56156a41ba867ced2830eab08ed1e3aa79ab2a2a
Polina Cheban <polic.che.13@gmail.com>
2021-04-27 18:57:09 +0300

    зробили завдання 6

--- js/script.js
+++ js/script.js
@@ -50,37 +50,130 @@ document.addEventListener('DOMContentLoaded', function(){
    }
    
    const timerTabs = setInterval(changeTab, 5000);
-     
+   
 
    const modalBtns = document.querySelectorAll('button[data-modal]'),
-        modal = document.querySelector('.modal'),
-        closeBtn = document.querySelector('.modal__close');
-    
-    
-    modalBtns.forEach((btn)=>{
-        btn.addEventListener('click', function(event){
-            modal.classList.add('show');
-            modal.classList.add('fade');
-            document.body.style.overflow = 'hidden';
-        });
+         modal = document.querySelector('.modal'),
+         closeBtn = document.querySelector('.modal__close');
 
-    });
-    function closeModal(event){
-        modal.classList.remove('show');
-        modal.classList.remove('fade');
-
-        document.body.style.overflow = 'auto';
-    }
-
-    closeBtn.addEventListener('click', closeModal);
-
-     modal.addEventListener('click', (event)=>{
-         if(event.target === modal){
-             closeModal();
-         }
-     });
- 
    
+
+   modalBtns.forEach((btn)=>{
+      btn.addEventListener('click', function(event){
+         modal.classList.add('show');
+         modal.classList.add('fade');
+         document.body.style.overflow = 'hidden';
+      });
+   });
+
+   function closeModal(event){
+      modal.classList.remove('show');
+      modal.classList.remove('fade');
+
+      document.body.style.overflow = 'auto';
+   }
+
+   closeBtn.addEventListener('click', closeModal);
+
+   modal.addEventListener('click', (event)=>{
+      if (event.target === modal){
+         closeModal();
+      }
+   });
+
+
+   const deadline = '2021-05-11';
+
+
+   function getTimeRemain(deadline){
+      endDate = Date.parse(deadline);
+      const now = new Date(),
+            t = endDate - now;
+
+
+      const days = Math.floor(t / 1000 / 60/ 60 / 24)  ;  
+      const hours = Math.floor((t / 1000 / 60 /60) % 24 );  
+      const minutes = Math.floor((t / 1000 / 60) % 60) ;  
+      const seconds = Math.floor((t / 1000) % 60) ;  
+
+      return {
+         'days': days,
+         'hours':hours,
+         'minutes': minutes,
+         'seconds':seconds,
+         'total': t
+      };
+   }
+
+   function setClock(endtime){
+      const timer = document.querySelector('.timer'),
+            days = document.getElementById('days'),
+            hours = document.getElementById('hours'),
+            minutes = document.getElementById('minutes'),
+            seconds = document.getElementById('seconds');           
+            
+
+            updateClock();
+
+            let timerUpdate = setInterval(updateClock, 1000);
+
+      function updateClock(){
+         const newTime = getTimeRemain(endtime);
+         
+         days.innerHTML = newTime.days;
+         hours.innerHTML = newTime.hours;
+         minutes.innerHTML = newTime.minutes;
+         seconds.innerHTML = newTime.seconds;
+
+         if (newTime.total<=0){
+            clearInterval(timerUpdate);
+
+         }
+      }
+   }
+
+   setClock(deadline);
+
+   
+   
+
+
+   
+   
+   const slider = document.querySelector('.offer__slider'),
+      slides = document.querySelectorAll('.offer__slide'),
+      prev = document.querySelector('.offer__slider-prev'),
+      next = document.querySelector('.offer__slider-next'),
+      total = document.querySelector('#total'),
+      current = document.querySelector('#current'),
+      sliderWrapper = document.querySelector('.offer__slider-wrapper');
+
+   const swiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+      
+      // Navigation arrows
+      navigation: {
+        nextEl: next,
+        prevEl: prev,
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+
+      
+
+         
+
+
+
+
+
 
 
 
